3030104e0ae2d4f826e302a7ee8a9776ffeb618f
spark <pnw123456@icloud.com>
2023-06-24 12:29:59 +0900

    test: 리뷰저장 테스트 케이스 추가 by domain layer

---
+++ review-service/src/test/kotlin/com/example/review/domain/ReviewWriterTest.kt
@@ -0,0 +1,42 @@
+package com.example.review.domain
+
+import com.example.review.infra.ReviewRepository
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.transaction.annotation.Transactional
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+open class ReviewWriterTest {
+
+    @Autowired
+    lateinit var reviewRepository: ReviewRepository
+
+    @Autowired
+    lateinit var reviewWriter: ReviewWriter
+
+    @Test
+    @Transactional
+    @Throws(Exception::class)
+    open fun `리뷰 저장 테스트`() {
+
+        //given
+        val review = Review(1L, "구매자1", "아주 좋은 상품입니다")
+
+        //when
+        val entity = reviewWriter.write(review)
+
+        //then
+        val findEntity = reviewRepository.findById(entity.id).orElseThrow()
+        assertEquals(entity, findEntity)
+        assertEquals(entity.productId, findEntity.productId)
+        assertEquals(entity.author, findEntity.author)
+        assertEquals(entity.createdAt, findEntity.createdAt)
+        assertEquals(entity.updatedAt, findEntity.updatedAt)
+    }
+
+}
