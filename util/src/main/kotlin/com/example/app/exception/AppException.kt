97c849dd30da28cd51f1e83741981665ad855bfc
spark <pnw123456@icloud.com>
2023-06-24 12:00:31 +0900

    feat: 글로벌에러헨들러 스팩 추가

--- util/src/main/kotlin/com/example/app/exception/AppException.kt
+++ util/src/main/kotlin/com/example/app/exception/AppException.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import kotlin.math.log
 
 /**
  * 어플리케이션내의 글로벌 핸들러
@@ -15,13 +16,23 @@ class GlobalExceptionHandler {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handler(e: Exception) =
+        AppExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.message)
+            .run {
+                logger.error(e.message, e)
+                this
+            }
+
     @ExceptionHandler(NotFoundDataException::class)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun handler(e: NotFoundDataException): AppExceptionMessage =
-        AppExceptionMessage(HttpStatus.NO_CONTENT.value(), e.message).let {
-            logger.warn("Not Found Data : {}", e.data)
-            it
-        }
+        AppExceptionMessage(HttpStatus.NO_CONTENT.value(), e.message)
+            .run {
+                logger.warn("Not Found Data : {}", e.data)
+                this
+            }
 
 }
 
